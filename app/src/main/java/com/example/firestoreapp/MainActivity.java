4bbd96debef66916f20d46bdcd045e4a0bc38094
DELL <omnianabil1920@gmail.com>
2024-07-22 11:42:57 +0300

    saving and reading the data

--- app/src/main/java/com/example/firestoreapp/MainActivity.java
+++ app/src/main/java/com/example/firestoreapp/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.firestoreapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
     private Button saveBtn;
@@ -36,6 +40,43 @@ public class MainActivity extends AppCompatActivity {
         deleteBtn = findViewById(R.id.deleteBTN);
         updateBtn = findViewById(R.id.updateBTN);
         readBtn = findViewById(R.id.readBTN);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveDataToNewDocument();
+            }
+        });
+         readBtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 getAllDocumentsInCollections();
+             }
+         });
 
+    }
+    private void saveDataToNewDocument(){
+        String name = nameET.getText().toString();
+        String email = emailET.getText().toString();
+        Friend friend = new Friend(name , email);
+        collectionReference.add(friend).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                String docId = documentReference.getId();
+            }
+        });
+    }
+    private void getAllDocumentsInCollections(){
+        collectionReference.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                String data = "";
+                for(QueryDocumentSnapshot snapshots : queryDocumentSnapshots){
+                    Friend friend = snapshots.toObject(Friend.class);
+                     data += "Name : " + friend.getName() + " Email : " + friend.getEmail() + "\n";
+                }
+                textView.setText(data);
+
+            }
+        });
     }
 }
